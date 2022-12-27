ac307114b6672b9baf419226b55b3eb44dae2205
samaneh <samanehmo1212@gmail.com>
2022-12-27 23:04:33 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useAppDispatch, useAppSelector } from './hooks/reduxHook'
+import { fetchAllProducts } from './redux/reducers/productReducers'
 
 const App = () => {
+  const products=useAppSelector(state=>state.productReducer)
+  console.log(products)
+  const dispatch=useAppDispatch()
+  useEffect(()=>{
+    dispatch(fetchAllProducts())
+  },[])
+
+  
   return (
     <div>App</div>
   )
